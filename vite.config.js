96558a4ed626fa1d26e44d2685ea9a5c53628215
jmrvicencio <35318735+jmrvicencio@users.noreply.github.com>
2026-02-24 19:33:20 +0800

    🔧 Updated config to use base-path

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'url';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
